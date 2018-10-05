f19858ddc499317d4585ad489d238f4611802ff7
Vishal <vishal.avalani@gmail.com>
2018-10-05 09:22:14 +0530

    Work on cities view start work on city view

--- src/Cities/Cities.js
+++ src/Cities/Cities.js
@@ -1,13 +1,50 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView, TouchableWithoutFeedback } from "react-native";
+
+import { colors } from "../theme";
 
 export default class Cities extends React.Component {
+  static navigationOptions = {
+    title: "Cities",
+    headerTitleStyle: {
+      color: "white",
+      fontSize: 20,
+      fontWeight: "400"
+    }
+  };
+  viewCity = city => {
+    this.props.navigation.navigate("City", { city });
+  };
+
   render() {
     console.log(this.props);
     return (
-      <View>
-        <Text>Hello from Cities</Text>
-      </View>
+      <ScrollView>
+        {this.props.screenProps.cities.map((city, index) => (
+          <View key={index}>
+            <TouchableWithoutFeedback onPress={() => this.viewCity(city)}>
+              <View style={styles.cityContainer}>
+                <Text style={styles.city}>{city.city}</Text>
+                <Text style={styles.country}>{city.country}</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        ))}
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  cityContainer: {
+    padding: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: colors.primary
+  },
+  city: {
+    fontSize: 20
+  },
+  country: {
+    color: "rgba(0,0,0,0.5)"
+  }
+});
--- src/Cities/City.js
+++ src/Cities/City.js
@@ -1,8 +1,29 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  TouchableWithoutFeedback,
+  TouchableOpacity
+} from "react-native";
+
+import { colors } from "../theme";
 
 export default class City extends React.Component {
+  static navigationOptions = props => {
+    return {
+      title: props.navigation.state.params.city.city,
+      headerTitleStyle: {
+        color: "white",
+        fontSize: 20,
+        fontWeight: "400"
+      }
+    };
+  };
   render() {
+    console.log(this.props);
     return (
       <View>
         <Text>Hello from City</Text>
